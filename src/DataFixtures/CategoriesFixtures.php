6d3439acf967d118b283918d1e8ee77b37d60047
baeteromain <baete.romain@gmail.com>
2021-06-17 18:02:04 +0200

    :heavy_plus_sign: add CategoriesFixtures #3

---
+++ src/DataFixtures/CategoriesFixtures.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoriesFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $categories = [
+          1 => [
+              'name' => 'Grabs'
+          ],
+          2 => [
+              'name' => 'Rotations'
+          ],
+          3 => [
+              'name' => 'Flips'
+          ],
+          4 => [
+              'name' => 'Rotations désaxées'
+          ],
+          5 => [
+              'name' => 'Slides'
+          ],
+          6 => [
+              'name' => 'One foot'
+          ],
+          7 => [
+              'name' => 'Old school'
+          ],
+        ];
+
+        foreach ($categories as $key => $value){
+            $category = new Category();
+            $category->setName($value['name']);
+
+            $manager->persist($category);
+
+            $this->addReference('category_' . $key, $category);
+        }
+
+        $manager->flush();
+    }
+}
