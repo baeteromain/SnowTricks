353ea01eff2f5ed99f61c729f89d340431d5c5b9
baeteromain <baete.romain@gmail.com>
2021-06-18 07:25:30 +0200

    :heavy_plus_sign: Add UsersFixture and base avatar for users #3

---
+++ src/DataFixtures/UsersFixtures.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UsersFixtures extends Fixture
+{
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $faker = Faker\Factory::create('fr_FR');
+
+        for($nbUsers = 1; $nbUsers <= 10; $nbUsers++){
+            $user = new User();
+
+            if ($nbUsers === 1){
+                $user->setEmail('admin@admin.fr');
+                $user->setPassword($this->encoder->encodePassword($user, 'admin'));
+                $user->setIsVerified(1);
+                $user->setRoles(['ROLE_ADMIN']);
+            } else {
+            $user->setRoles(['ROLE_USER']);
+            $user->setEmail($faker->email);
+            $user->setPassword($this->encoder->encodePassword($user, 'azerty'));
+            $user->setIsVerified($faker->numberBetween(0,1));
+            }
+            $user->setUsername($faker->userName);
+            $user->setAvatar('Multiavatar-' . $faker->numberBetween(1,4));
+
+
+            $manager->persist($user);
+
+            $this->addReference('user_' . $nbUsers, $user);
+        }
+
+        $manager->flush();
+    }
+}
