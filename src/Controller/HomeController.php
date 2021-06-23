886c042d167091a6ea9a5bace88744476c7b592a
baeteromain <baete.romain@gmail.com>
2021-06-23 16:22:10 +0200

    :heavy_plus_sign: Add new HomeController #4

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\TrickRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_home")
+     */
+    public function index(TrickRepository $trickRepository): Response
+    {
+        $tricks = $trickRepository->findBy([], ['created_at' => 'desc'], 15, 0);
+
+        return $this->render('home/index.html.twig', [
+            'tricks' => $tricks,
+        ]);
+    }
+}
