3edc3ca8addf69f9cce0051b535c20cff68395ca
baeteromain <baete.romain@gmail.com>
2021-07-07 10:31:51 +0200

    Fix ajax request with button load more #4

--- assets/modules/LoadMoreTrick.js
+++ assets/modules/LoadMoreTrick.js
@@ -6,14 +6,19 @@ export default class LoadMoreTrick {
         this.loadMore = element.querySelector('#load_more')
         this.trick = element.querySelector('#tricks')
         this.trickCard = element.querySelector('#tricks_cards')
+        this.card_trick = document.querySelectorAll('#card_trick')
+        console.log(this.card_trick.length)
         this.bindEvents()
     }
 
     bindEvents() {
-        this.loadMore.querySelectorAll('a').forEach(a => {
-            a.addEventListener('click', e => {
-                e.preventDefault()
-                this.loadUrl(a.getAttribute('href') )
+        let nbTrick = 5
+        let click = 0
+        this.loadMore.querySelectorAll('button').forEach(b => {
+            b.addEventListener('click', () => {
+                click++
+                let offset = nbTrick * click
+                this.loadUrl('/ajax/' + offset )
             })
         })
     }
@@ -26,7 +31,12 @@ export default class LoadMoreTrick {
         })
         if(response.status >= 200 && response.status < 300){
             const data = await response.json()
+            if(data.content === false){
+                document.getElementById('spinner').style.display = "none"
+                document.getElementById('load').style.display = "none"
+            }else{
             this.trickCard.insertAdjacentHTML('beforeend', data.content)
+            }
         } else {
             console.error(response)
         }
