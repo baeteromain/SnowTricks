7ab6b8996ea14503746b9421be0014510a7a579f
baeteromain <baete.romain@gmail.com>
2021-07-07 14:01:44 +0200

    Add form trick #5

---
+++ src/Form/TrickType.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Trick;
+use App\Entity\User;
+use App\Entity\Video;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TrickType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name'
+            ])
+            ->add('description', TextareaType::class)
+            ->add('images', FileType::class, [
+                'label' => 'Image(s)',
+                'multiple' => true,
+                'mapped' => false,
+                'required' => true,
+            ])
+            ->add('videos', CollectionType::class,[
+                'entry_type' => VideoType::class,
+                'label' => 'Lien Vidéo',
+                'entry_options' => ['label' => false],
+                'allow_add' => true,
+                'allow_delete' => true,
+                'by_reference' => false,
+            ])
+            ->add('Valider', SubmitType::class);
+
+
+
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Trick::class,
+        ]);
+    }
+}
