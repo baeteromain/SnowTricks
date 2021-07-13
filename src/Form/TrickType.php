7eb3cf9f06a04e712e49a13764ac50a479800ffb
baeteromain <baete.romain@gmail.com>
2021-07-13 15:30:50 +0200

    Refactor form with collectiontype for images and videos #5

--- src/Form/TrickType.php
+++ src/Form/TrickType.php
@@ -4,12 +4,9 @@ namespace App\Form;
 
 use App\Entity\Category;
 use App\Entity\Trick;
-use App\Entity\User;
-use App\Entity\Video;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -28,11 +25,14 @@ class TrickType extends AbstractType
                 'choice_label' => 'name'
             ])
             ->add('description', TextareaType::class)
-            ->add('images', FileType::class, [
-                'label' => 'Image(s)',
-                'multiple' => true,
-                'mapped' => false,
-                'required' => true,
+
+            ->add('images', CollectionType::class,[
+                'entry_type' => ImageType::class,
+                'label' => 'Images',
+                'entry_options' => ['label' => false],
+                'allow_add' => true,
+                'allow_delete' => true,
+                'by_reference' => false,
             ])
             ->add('videos', CollectionType::class,[
                 'entry_type' => VideoType::class,
@@ -43,9 +43,6 @@ class TrickType extends AbstractType
                 'by_reference' => false,
             ])
             ->add('Valider', SubmitType::class);
-
-
-
 
         ;
     }
